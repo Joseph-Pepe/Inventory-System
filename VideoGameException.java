249e8e04c4c01c74480f46db5a027141f86cc187
PEPMASTER <32807576+PEPMASTER@users.noreply.github.com>
2019-08-18 17:11:12 -0400

    Exception class used in the VideoGame.java file.

--- VideoGameException.java
+++ VideoGameException.java
@@ -1,14 +1,11 @@
-public class VideoGameException extends Exception
-{
+public class VideoGameException extends Exception{
 	private String message;
 
-	public VideoGameException(String newMessage)
-	{
+	public VideoGameException(String newMessage){
 		message = newMessage;
 	}
 
-	public String toString()
-	{
+	public String toString(){
 		return message;
 	}
 }
